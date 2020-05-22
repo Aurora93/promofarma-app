8974379d300d548297654c3b9f657b1744a2a325
Aurora Pérez Alonso <aurora.perez1993@gmail.com>
2020-05-22 17:27:56 +0200

    add initial data code

---
+++ public/data/data.js
@@ -0,0 +1,32 @@
+const productList = [
+    {
+        name: "LaJusticia colágeno con magnesio 450comp",
+        image: "../product-images/lajusticia-colageno.jpg",
+        price: 14.35
+    },
+    {
+        name: "Xhekpon® crema facial 40ml",
+        image: "../product-images/xhekpon-crema.jpg",
+        price: 6.49
+    },
+    {
+        name: "Cerave® Crema Hidratante 340ml",
+        image: "../product-images/cerave-crema.jpg",
+        price: 11.70
+    },
+    {
+        name: "Hyabak solución 10ml",
+        image: "../product-images/hyabak-solucion.jpg",
+        price: 9.48
+    },
+    {
+        name: "Fotoprotector ISDIN® Fusion Water SPF 50+ 50ml",
+        image: "../product-images/fotoprotector-isdin.jpg",
+        price: 19.74
+    },
+    {
+        name: "Piz Buin® ALlergy SPF50+ loción 200ml",
+        image: "../product-images/piz-buin.jpg",
+        price: 8.65
+    }
+];
