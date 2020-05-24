72a8dd54e335c4688972a2ce9fcd50e4fcfb63dd
Aurora Pérez Alonso <aurora.perez1993@gmail.com>
2020-05-24 11:30:27 +0200

    componetize sass

--- src/components/cart-item.js
+++ src/components/cart-item.js
@@ -2,34 +2,34 @@
 
 function CartItem(props) {
     var cartedProduct = document.createElement("li");
-    cartedProduct.classList.add("cart__product");
+    cartedProduct.classList.add("cart-item");
     Component.call(this, cartedProduct);
 
     var cartLeft = document.createElement("div");
-    cartLeft.classList.add("cart__left");
+    cartLeft.classList.add("cart-item__left");
 
     var cartImage = document.createElement("img");
-    cartImage.classList.add("cart__image");
+    cartImage.classList.add("cart-item__image");
     cartImage.src = "../../public/product-images/" + props.result.image;
     cartLeft.append(cartImage);
 
     var cartName = document.createElement("p");
-    cartName.classList.add("cart__name");
+    cartName.classList.add("cart-item__name");
     cartName.innerText = props.result.name;
     cartLeft.append(cartName);
 
     cartedProduct.append(cartLeft);
 
     var cartPrice = document.createElement("div");
-    cartPrice.classList.add("cart__price");
+    cartPrice.classList.add("cart-item__price");
 
     var cartPriceValue = document.createElement("span");
-    cartPriceValue.classList.add("cart__price-value");
+    cartPriceValue.classList.add("cart-item__price-value");
     cartPriceValue.innerText = props.result.price + " €";
     cartPrice.append(cartPriceValue);
 
     var cartDeleteButton = document.createElement("button");
-    cartDeleteButton.classList.add("cart__delete-button");
+    cartDeleteButton.classList.add("cart-item__delete-button");
     cartDeleteButton.innerText = "Eliminar";
     cartDeleteButton.addEventListener("click", function(event) {
         event.preventDefault();
--- src/components/product.js
+++ src/components/product.js
@@ -2,27 +2,27 @@
 
 function Product(props) {
     var product = document.createElement("li");
-    product.classList.add("product-list__product");
+    product.classList.add("product");
     Component.call(this, product);
     
     var productName = document.createElement("p");
-    productName.classList.add("product-list__name");
+    productName.classList.add("product__name");
     productName.innerText = props.result.name;
     product.append(productName);
 
     var productPrice = document.createElement("p");
-    productPrice.classList.add("product-list__price");
+    productPrice.classList.add("product__price");
     productPrice.innerText = props.result.price + " €";
     product.append(productPrice);
 
     var addToCartButton = document.createElement("img");
-    addToCartButton.classList.add("product-list__add-to-cart");
+    addToCartButton.classList.add("product__add-to-cart");
     addToCartButton.src = "../../public/icons/add-to-cart.png";
     addToCartButton.addEventListener("click", function(event) {
         event.preventDefault();
-        // if (addToCartButton.className === "product-list__add-to-cart") addToCartButton.className += "--disabled";
-        // else addToCartButton.className = "product-list__add-to-cart";
-        if (addToCartButton.className === "product-list__add-to-cart") {
+        // if (addToCartButton.className === "product__add-to-cart") addToCartButton.className += "--disabled";
+        // else addToCartButton.className = "product__add-to-cart";
+        if (addToCartButton.className === "product__add-to-cart") {
             addToCartButton.className += "--disabled";
             productPrice.className += "--disabled";
         } else return;
