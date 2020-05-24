0f0525d93b2affa1550c64c02e88bcf750b9356a
Aurora Pérez Alonso <aurora.perez1993@gmail.com>
2020-05-24 17:34:10 +0200

    fix styles

--- src/components/product-list.js
+++ src/components/product-list.js
@@ -27,11 +27,11 @@ function ProductList(props){
 ProductList.extendsFrom(Component);
 
 ProductList.prototype.enableAddToCart = function(productId) {
-    document.getElementById(productId).querySelector(".product-list__add-to-cart--disabled").className = "product-list__add-to-cart";
-    document.getElementById(productId).querySelector(".product-list__price--disabled").className = "product-list__price";
+    document.getElementById(productId).querySelector(".product__add-to-cart--disabled").className = "product__add-to-cart";
+    document.getElementById(productId).querySelector(".product__price--disabled").className = "product__price";
 }
 
 ProductList.prototype.disableAddToCart = function(productId) {
-    document.getElementById(productId).querySelector(".product-list__add-to-cart").className = "product-list__add-to-cart--disabled";
-    document.getElementById(productId).querySelector(".product-list__price").className = "product-list__price--disabled";
+    document.getElementById(productId).querySelector(".product__add-to-cart").className = "product__add-to-cart--disabled";
+    document.getElementById(productId).querySelector(".product__price").className = "product__price--disabled";
 }
